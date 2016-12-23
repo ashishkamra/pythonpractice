af568fb503b60f22ace3cad9648cbb372c88a948
Ashish Kamra <kamraa@localhost.localdomain>
2016-12-23 12:55:49 -0500

    completed exercise

--- google-python-exercises/basic/wordcount.py
+++ google-python-exercises/basic/wordcount.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python -tt
+#!/usr/bin/python2.7 -tt
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
@@ -46,6 +46,84 @@ import sys
 # Then print_words() and print_top() can just call the utility function.
 
 ###
+# print words in filename sorted by frequency(count)
+def print_words(filename):
+  # create a dictionary of word.lower() - count
+  f_dict = file_dict(filename)
+
+  #sort based on key
+  words = sorted(f_dict)
+
+  #sort based on key length
+  words_l = sorted(f_dict, key = len)  
+
+  #print them nicely with padding. use the maximum key length for padding
+  print 'word'.ljust(len(words_l[-1])), ' count' 
+  print '----'.ljust(len(words_l[-1])), ' -----' 
+
+  # print out the sorted words list
+  for word in words:
+    print word.ljust(len(words_l[-1])), ' ', f_dict[word]
+  
+  return 
+
+# print top 20 words in filename sorted by frequency(count)
+def print_top(filename):
+  # create a dictionary of word.lower() - count
+  f_dict = file_dict(filename)
+
+  #sort based on values (got it on stackoverflow)
+  words = sorted(f_dict, key = f_dict.get)
+
+  #sort based on key length
+  words_l = sorted(f_dict, key = len)  
+
+  #print them nicely with padding. use the maximum key length for padding
+  print 'word'.ljust(len(words_l[-1])), ' count' 
+  print '----'.ljust(len(words_l[-1])), ' -----' 
+
+  # print out the top 20 sorted words list
+  num = 0
+  pval = ''
+  for word in words:
+    # logic: keep count of when the values in the sorted list change. Stop after it has changed 20 times. 
+    cval = f_dict[word]
+    if pval != cval:
+      num += 1
+    if num < 20:
+      print word.ljust(len(words_l[-1])), ' ', cval
+      pval = cval
+    else:
+      break
+  
+  return
+
+# Helper function to return a dictionary of word/count given a file name
+def file_dict(filename):
+
+  # Initialize empty dictionary
+  f_dict = {}
+
+  # open the file handle for filename in read-only mode
+  try:
+    f_h = open(filename, 'r')
+  except(IOError):
+    print 'Please provide a file that exists'
+    return
+
+  #build the dictionary by iterating over all the lines
+  for line_str in f_h:
+    #split the line into words by whitespace
+    line_split = line_str.split()
+    for word in line_split:
+      if word.lower() in f_dict:
+        f_dict[word.lower()] += 1
+      else:
+        f_dict[word.lower()] = 1
+   
+  #print f_dict
+
+  return f_dict
 
 # This basic command line argument parsing code is provided and
 # calls the print_words() and print_top() functions which you must define.
