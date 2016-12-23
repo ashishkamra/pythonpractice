9a80f99e7261ea4c95b25fc15378804d1a93fcb4
Ashish Kamra <kamraa@localhost.localdomain>
2016-12-23 17:22:57 -0500

    completed exercises

--- google-python-exercises/babynames/babynames.py
+++ google-python-exercises/babynames/babynames.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2.7  
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
@@ -41,7 +41,35 @@ def extract_names(filename):
   ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
   """
   # +++your code here+++
-  return
+  f_h = open(filename, 'r')
+
+  #reg-ex for rank, male name, female name
+  other_re = '(<tr align="right"><td>)\d(</td><td>)([a-z][A-Z]+)(</td><td>)([a-z][A-Z]+)(</td>)'
+
+  year_match = True
+  f_dict = {}
+  for line in f_h:
+    #print line
+    if year_match:
+      match_y = re.search(r'(Popularity in) (\d\d\d\d)', line)
+
+    if match_y and year_match:
+      year = match_y.group(2)
+      year_match = False
+      #print year
+
+    match_o = re.search(r'(<tr align="right"><td>)(\d?\d?\d?\d?)(</td><td>)(\w*)(</td><td>)(\w*)', line)
+    if match_o:
+      rank = match_o.group(2)
+      name_male = match_o.group(4)
+      name_female = match_o.group(6)
+      f_dict[name_male] = rank
+      f_dict[name_female] = rank
+
+  sorted_list_names = sorted(f_dict.items())
+  sorted_list_names.insert(0,(year))
+
+  return sorted_list_names
 
 
 def main():
@@ -49,7 +77,6 @@ def main():
   # Make a list of command line arguments, omitting the [0] element
   # which is the script itself.
   args = sys.argv[1:]
-
   if not args:
     print 'usage: [--summaryfile] file [file ...]'
     sys.exit(1)
@@ -58,11 +85,21 @@ def main():
   summary = False
   if args[0] == '--summaryfile':
     summary = True
+    sf_h = open('summary.txt','w+')
     del args[0]
 
   # +++your code here+++
   # For each filename, get the names, then either print the text output
   # or write it to a summary file
-  
+  for f in args:
+    summary_list = extract_names(f)  
+    if summary == True:
+      print >> sf_h, summary_list
+    else:
+      print summary_list
+
+  if summary == True:
+    sf_h.close()
+
 if __name__ == '__main__':
   main()
