2c851c0b143d8d0b6865842931b90fe28ce9bca8
Ashish Kamra <kamraa@localhost.localdomain>
2016-12-23 19:13:34 -0500

    completed exercise

--- google-python-exercises/copyspecial/copyspecial.py
+++ google-python-exercises/copyspecial/copyspecial.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2.7 -tt
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
@@ -18,7 +18,60 @@ import commands
 # +++your code here+++
 # Write functions and modify main() to call them
 
+# returns a list of absolute paths for special files in the given directory
+def get_special_paths(dirs):
 
+  abs_paths = []
+  for d in dirs:
+    filenames = os.listdir(d)
+    for f in filenames:
+      match = re.search(r'\w*__\w*__\w*.\w*', f)
+      if match:
+        abs_paths.append(os.path.abspath(f))
+      
+  return abs_paths
+
+# copies files indicated by abs_paths to todir directory
+def copy_files(abs_paths, todir):
+
+  # create todir if not exists
+  if not os.path.exists(todir):
+    try:
+      os.mkdir(todir)
+    except:
+      print 'Dir creation error:', sys.exc_info()[0], sys.exc_info()[1]
+      sys.exit(-1)
+
+  # copy files
+  for path in abs_paths:
+    try:
+      shutil.copy(path, todir)
+    except:
+      print 'File copy error:', sys.exc_info()[0], sys.exc_info()[1]
+      sys.exit(-1)
+
+  return
+
+# zips up files pointed by abs_paths using an external zip command 
+def zip_files(abs_paths, tozip):
+  
+  # form the external command
+  (status, cmd_path) = commands.getstatusoutput('which zip')
+  if status != 0:
+    print 'zip command does not exist on this platform. Unable to proceed!'
+    sys.exit(status)
+  
+  cmd = cmd_path + ' -r ' + tozip + ' '+ ' '.join(abs_paths)
+  
+  print cmd
+
+  (status, output) = commands.getstatusoutput(cmd)
+  
+  if status != 0:
+    print 'Unable to zip - output:', output
+    sys.exit(status)
+
+  return
 
 def main():
   # This basic command line argument parsing code is provided.
@@ -51,5 +104,21 @@ def main():
   # +++your code here+++
   # Call your functions
   
+  # get absolute paths from the given input dirs
+  # print them if no todir and tozip options are provided
+  dirs = args
+  abs_paths = get_special_paths(dirs)
+  if todir == '' and tozip == '':
+    for p in abs_paths: print(p)
+
+  # copy the files to the destination directory if todir is set
+  # create the dir if needed
+  if todir != '':
+    copy_files(abs_paths, todir)
+
+  #zip files to a new zip file if tozip is set
+  if tozip != '':
+    zip_files(abs_paths, tozip)  
+
 if __name__ == "__main__":
   main()
