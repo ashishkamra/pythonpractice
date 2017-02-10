b8f01d66978ffd2cad33858d582383c5e53ce232
Ashish Kamra <kamraa@lhmc4247.lss.emc.com>
2017-02-10 18:52:57 -0500

    added functions

--- binary_search.py
+++ binary_search.py
@@ -1,10 +1,12 @@
-#! /home/kamraa/anaconda3/bin/python3 -tt
+#! $HOME/anaconda3/bin/python3 -tt
+
+import random
 
 # Node class
 # Class Variables - 
 # Attributes - left_node, right_node, data (int)
 # Methods - constructor, destructor
-class node:
+class a_node:
   # Class Variables
   init_val = 0
 
@@ -22,24 +24,61 @@ class BinaryTree:
 
   # init
   def __init__(self, data):
-    self.top_node = node(data)
+    self.top_node = a_node(data)
     self.num_nodes = 1
+    self.height = 1
 
   # init
   def __init__(self):
     self.top_node = None
     self.num_nodes = 0
+    self.height = 0
+
+  # in-oder printing - sorted
+  def process_in(self, node):
+    if node.left_node is not None:
+      self.process_in(node.left_node)
+
+    print(node.data)
+
+    if node.right_node is not None:
+      self.process_in(node.right_node)
+  
+  #pre-order printing
+  def process_pre(self, node):
+    print(node.data)
+
+    if node.left_node is not None:
+      self.process_pre(node.left_node)
+
+    if node.right_node is not None:
+      self.process_pre(node.right_node)
+
+  # post-order printing
+  def process_post(self, node):
+    
+    if node.left_node is not None:
+      self.process_post(node.left_node)
+
+    if node.right_node is not None:
+      self.process_post(node.right_node)
+
+    print(node.data)
 
   # insert a new node
   # Return: 0 - Success, anything else - Failure
   def insert(self, in_node):
 
     node = self.top_node
+    height = 0
 
     # first node - easy
     if node is None:
       self.top_node = in_node
-      #print("Node inserted "+str(in_node.data))
+      #print("Top node inserted "+str(in_node.data))
+      self.num_nodes += 1
+      height += 1
+      self.height = height
       return 0    
 
     # traverse from top_node to find the right location to insert the node
@@ -53,10 +92,13 @@ class BinaryTree:
         if node.left_node is None:         # found the place to insert
           node.left_node = in_node
           self.num_nodes += 1
-          #print("Node inserted "+str(in_node.data))
+          #print("Node inserted to left "+str(in_node.data))
+          if height > self.height:
+            self.height = height
           return 0
-        else:                              # go one
+        else:                              # go on
           node = node.left_node
+          height += 1
           continue;
 
       # go right
@@ -64,64 +106,79 @@ class BinaryTree:
         if node.right_node is None:       # found the place to insert
           node.right_node = in_node
           self.num_nodes += 1
-          #print("Node inserted "+str(in_node.data))
+          #print("Node inserted to right "+str(in_node.data))
+          if height > self.height:
+            self.height = height
           return 0
-        else:                             # go one
+        else:                             # go on
           node = node.right_node
+          height += 1
           continue;      
     
   # delete a node
   # Return: 0 - Success, anything else - Failure
   def delete(self, node):
+    #if self.find(node.data) is not None:
     pass
+      
+
+  # internal function for recursion
+  def find_in(self, data, node):
+
+    print("checked "+str(node.data))
+
+    ret_val = None
+
+    if node.data == data:     #found
+      return node
+    elif node.left_node is None and node.right_node is None: #not found
+      return None
+
+    #go left
+    if node.left_node is not None and data < node.data:
+      ret_val = self.find_in(data, node.left_node)
+      if ret_val is not None:
+        return ret_val
+
+    #go right
+    if node.right_node is not None and data > node.data:
+      ret_val = self.find_in(data, node.right_node)  
+      if ret_val is not None:
+        return ret_val
+
+    return ret_val
+
   # search for a node
-  # Return: node if found, a negative number otherwise
-  def find(self, node):
-    pass
+  # Return: node if found, None otherwise
+  def find(self, data):
+
+    # start at top
+    node = self.top_node
+    return self.find_in(data, node) 
 
   # print the entire tree structure
   # order - pre-order, post order, in-order
   # Return: 0 - Success, anything else - Failure
   def print(self, order):
 
+    node = self.top_node
+
+    if node is None:
+      print("Empty Tree. Nothing to print")
+      return -1
+
     if order == 'in':  
-      node = self.top_node
-
-      if node is None:
-        print("Empty Tree. Nothing to print")
-        return -1
-
-      stack = []
-      from_stack = False
-      while True:
-
-        if from_stack and node.right_node is not None:
-          print(node)
-          stack.pop()
-          if len(stack) != 0:
-            node = stack[len(stack) -1]
-            from_stack = True
-            continue;                    
-          else:
-            # if not from stack or has a right node put the nodes on the stack
-            if node.right_node is not None:
-              stack.append(node.right_node)      
-            if not from_stack:
-              stack.append(node)        
-
-        if node.left_node is not None:
-          node = node.left_node
-          from_stack = False
-          continue;
-        else:
-          print(node)
-          stack.pop()
-          if len(stack) != 0:
-            node = stack[len(stack) -1]
-            from_stack = True
-            continue;                    
-          else:
-            return 0
+      self.process_in(node)
+      return 0
+    elif order == 'pre':
+      self.process_pre(node)
+      return 0
+    elif order == 'post':
+      self.process_post(node)   
+      return 0
+    else:
+      print("Incorrect option")
+      return -1
 
 def main():
 
@@ -132,19 +189,49 @@ def main():
     print("file does not exist")
     return -1
   
-  # create an empty tree to begin with
+  # create an empty tree
   tree = BinaryTree()
 
-  # create a binary tree by adding nodes as you read the numbers
-  print("printing the numbers as they are being read")
-  for num in f:
-    print(num,)
-    n = node(num)
-    tree.insert(n)
+  # list comprehension to read the numbers in a list
+  num_list = [num for num in f]
+  
+  # randomize to get balanced tree
+  random.shuffle(num_list)
+
+  # insert numbers one by one
+  for num in num_list:
+    tree.insert(a_node(int(num)))
     
-  # print the tree
-  print("printing the tree")
+  # print the tree and vital stats
+  print("tree height = "+str(tree.height))
+  print("tree nodes = "+str(tree.num_nodes))
+  print("In-order printing the tree")
   tree.print("in")
+
+  #print("pre-order printing the tree")
+  #tree.print("pre")
+  
+  #print("post-order printing the tree")
+  #tree.print("post")
+
+  #num_find = 76
+  #if tree.find(int(num_find)) is not None:
+  #  print("found "+str(num_find))
+  #else:
+  #  print("not found "+str(num_find))
+
+  num_delete = 76
+  if tree.delete(int(num_delete)) == 0:
+    print("deleted "+str(num_delete))
+  else:
+    print("could not delete the number")
+
+  # print the tree and vital stats
+  print("tree height = "+str(tree.height))
+  print("tree nodes = "+str(tree.num_nodes))
+  print("In-order printing the tree")
+  tree.print("in")
+
 
 if __name__ == '__main__':
   main()
